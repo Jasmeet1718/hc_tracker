7e69f31daac14fdd1e486a4320de97c387f81a55
Mohamed Saleem <67532838+SaleemIlahi@users.noreply.github.com>
2022-11-14 17:22:56 +0530

    Update tracker.sql

--- tracker.sql
+++ tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2022 at 05:02 AM
+-- Generation Time: Nov 14, 2022 at 12:51 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -106,6 +106,68 @@ INSERT INTO `tracker_data` (`id`, `campaign_name`, `client_name`, `agency_name`,
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `team` varchar(15) NOT NULL,
+  `role` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `team`, `role`) VALUES
+(1, 'mohamed.s@hockeycurve.com', 'garud', 'Garud', 0),
+(2, 'sapna.g@hockeycurve.com', 'nischay', 'Nischay', 0),
+(3, 'vishwadeep.sonawale@hockeycurve.com', 'gati', 'Gati', 1),
+(6, 'jasmeet.singh@hockeycurve.com', 'garud', 'Garud', 0),
+(7, 'mahesh.b@hockeycurve.com', 'nischay', 'Nischay', 0),
+(8, 'mridul@hockeycurve.com', 'nischay', 'Nischay', 0),
+(9, 'pavan.vichare@hockeycurve.com', 'garud', 'Garud', 0),
+(10, 'pooja@hockeycurve.com', 'nischay', 'Nischay', 0),
+(11, 'adithi@hockeycurve.com', 'nischay', 'Nischay', 0),
+(12, 'aneeshk@hockeycurve.com', 'garud', 'Garud', 0),
+(13, 'prathamesh.bhagwat@hockeycurve.com', 'garud', 'Garud', 0),
+(14, 'nikhil.g@hockeycurve.com', 'garud', 'Garud', 0),
+(15, 'nikita.d@hockeycurve.com', 'veena@', 'Veena', 3),
+(16, 'siddhi.phalke@hockeycurve.com', 'veena@', 'Veena', 3),
+(17, 'raj.p@hockeycurve.com', 'gati', 'Gati', 1),
+(18, 'hariharanv@hockeycurve.com', 'veena@', 'Veena', 3),
+(19, 'tejshwini@hockeycurve.com', 'veena@', 'Veena', 3),
+(20, 'apowar@hockeycurve.com', 'veena@', 'Veena', 3),
+(21, 'ninad.ganu@hockeycurve.com', 'veena@', 'Veena', 3),
+(22, 'bavika@hockeycurve.com', 'veena@', 'Veena', 3),
+(23, 'dinesh@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(24, 'priya.rajput@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(25, 'naved.chowdhary@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(26, 'manish.madhavi@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(27, 'tanishq', 'tanishq', 'tanishq', 2),
+(28, 'shivam.singh@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(29, 'shivam.singh@hockeycurve.com', 'gati', 'Gati', 1),
+(30, 'aditya@hockeycurve.com', 'gati', 'Gati', 1),
+(31, 'aditya@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(32, 'sai@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(33, 'sai@hockeycurve.com', 'gati', 'Gati', 1),
+(34, 'sai@hockeycurve.com', 'veena@', 'Veena', 3),
+(35, 'harsh@hockeycurve.com', 'veena@', 'Veena', 3),
+(36, 'harsh@hockeycurve.com', 'gati', 'Gati', 1),
+(37, 'harsh@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(38, 'sravan@hockeycurve.com', 'lakshay', 'Lakshay', 0),
+(39, 'sravan@hockeycurve.com', 'veena@', 'Veena', 3),
+(40, 'sravan@hockeycurve.com', 'gati', 'Gati', 1),
+(41, 'aditya@hockeycurve.com', 'veena@', 'Veena', 3),
+(42, 'dinesh@hockeycurve.com', 'veena@', 'Veena', 3),
+(43, 'dinesh@hockeycurve.com', 'gati', 'Gati', 1),
+(44, 'shivam.singh@hockeycurve.com', 'veena@', 'Veena', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `veena_team`
 --
 
@@ -114,17 +176,18 @@ CREATE TABLE `veena_team` (
   `name` varchar(255) NOT NULL,
   `task` varchar(255) NOT NULL,
   `time` int(20) NOT NULL,
-  `assets` varchar(255) NOT NULL
+  `assets` varchar(255) NOT NULL,
+  `mail_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `veena_team`
 --
 
-INSERT INTO `veena_team` (`id`, `name`, `task`, `time`, `assets`) VALUES
-(1, 'Hari', '', 0, ''),
-(2, 'Siddhi', 'demo ', 16200, ''),
-(3, 'Amar', 'jasmeet124', 5400, '');
+INSERT INTO `veena_team` (`id`, `name`, `task`, `time`, `assets`, `mail_id`) VALUES
+(1, 'Hari', '', 0, '', 'hariharanv@hockeycurve.com'),
+(2, 'Siddhi', 'demo ', 16200, '', 'siddhi.phalke@hockeycurve.com'),
+(3, 'Amar', 'jasmeet124', 5400, '', 'apowar@hockeycurve.com');
 
 --
 -- Indexes for dumped tables
@@ -140,6 +203,12 @@ ALTER TABLE `hc_templates`
 -- Indexes for table `tracker_data`
 --
 ALTER TABLE `tracker_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -163,6 +232,12 @@ ALTER TABLE `hc_templates`
 --
 ALTER TABLE `tracker_data`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `veena_team`
