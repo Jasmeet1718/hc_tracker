54c2261602eec8938d2c7f802ef18f197aade88a
Jasmeet1718 <65601510+Jasmeet1718@users.noreply.github.com>
2022-11-16 15:50:49 +0530

    Delete tracker directory

--- tracker/tracker_update/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../conn.php";
-    $sno=$_GET['sno'];
-    $sql="DELETE FROM `hc_templates` WHERE sno=$sno";
-    $result=mysqli_query($connectDB,$sql);
-    if ($result){
-        header("location:index.php");
-    }
-?>
--- tracker/veena_end.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "conn.php";
-    $id=$_GET['id'];
-    $sql="UPDATE `veena_team` SET `task`='' ,`time`='0' WHERE id=$id LIMIT 1";
-    $result=mysqli_query($connectDB,$sql);
-    
-?>
