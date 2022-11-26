2840ec0a969a89c9a3b290c41e8ddc02a83df891
Jasmeet1718 <65601510+Jasmeet1718@users.noreply.github.com>
2022-11-26 06:42:05 +0530

    Uploaded the same code as last night no changes

--- tracker/conn.php
+++ tracker/conn.php
@@ -13,7 +13,6 @@ if (!$connectDB){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script> -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
--- tracker/veena/asset_v.php
+++ tracker/veena/asset_v.php
@@ -13,39 +13,47 @@ error_reporting(E_ERROR | E_PARSE);
 date_default_timezone_set('Asia/Kolkata'); // india time setting 
 $time = date('Y-m-d H:i:s');
 
-if(isset($_POST["submit"])){
-    $assets=$_POST['assets'];
-    $sql101="SELECT * FROM `veena_team` WHERE id = $id";
-        $result101=mysqli_query($connectDB,$sql101);
-        $row101=mysqli_fetch_assoc($result101);
-        $task=$row101["task"];
-        $tt=explode("--",$task);
-        $task_name=$tt[0];
-        $temp_tbd=$tt[1];
-        $member=$row101["name"];
-        $assign_time=$row101["assign_time"];
-        $final_time=date("Y-m-d H:i:s");
-        $deviation=strtotime($final_time)-strtotime($assign_time)-$row101["time"];
-        if ($deviation<0){
-            $deviation=0;
+    if(isset($_POST["submit"])){
+        $input1=$_POST['input1'];
+        $assets=$_POST['assets-0'];
+        $tt=explode(",",$input1);
+        for ($i=0;$i<count($tt);$i++){
+            $assets.=",".$_POST[$tt[$i]]; 
         }
-    $sql="UPDATE `assign_task` SET `deviation_v`='$deviation',`status_v`='2',`member`='$member',`assign_time`='$assign_time',`assets`='$assets' WHERE `task_name`='$task_name' AND `temp_tbd`='$temp_tbd' AND `assign_to`='Veena'";
-    $result=mysqli_query($connectDB,$sql);
-    $sqld="DELETE FROM `veena_team` WHERE id=$id";
-    $resultd=mysqli_query($connectDB,$sqld);
-    if($result){
-        header("location: view_v.php");
-    }
-
-    $assign="SELECT * FROM `hc_templates` WHERE `template`='$temp_tbd'";
-    $result_assign=mysqli_query($connectDB,$assign);
-    $team=mysqli_fetch_assoc($result_assign);
-    $development_assign=$team["development_assign"];
-    $time_req_dev=$team["time_req_dev"];
-
-    $sqlg="INSERT INTO `assign_task`(`task_name`, `temp_tbd`, `assign_to`, `time_v`, `status_v`,`assign_time`,`assets`) VALUES ('$task_name','$temp_tbd','$development_assign','$time_req_dev','0','$time','$assets')";
-    $resultg=mysqli_query($connectDB,$sqlg);
+        // echo $assets;
+        $sql101="SELECT * FROM `veena_team` WHERE id = $id";
+            $result101=mysqli_query($connectDB,$sql101);
+            $row101=mysqli_fetch_assoc($result101);
+            $task=$row101["task"];
+            $tt=explode("--",$task);
+            $task_name=$tt[0];
+            $temp_tbd=$tt[1];
+            $member=$row101["name"];
+            $assign_time=$row101["assign_time"];
+            $final_time=date("Y-m-d H:i:s");
+            $deviation=strtotime($final_time)-strtotime($assign_time)-$row101["time"];
+            if ($deviation<0){
+                $deviation=0;
+            }
+        $sql="UPDATE `assign_task` SET `deviation_v`='$deviation',`status_v`='2',`member`='$member',`assign_time`='$assign_time',`assets`='$assets' WHERE `task_name`='$task_name' AND `temp_tbd`='$temp_tbd' AND `assign_to`='Veena'";
+        $result=mysqli_query($connectDB,$sql);
+        $sqld="DELETE FROM `veena_team` WHERE id=$id";
+        $resultd=mysqli_query($connectDB,$sqld);
+        if($result){
+            
+        }
+    
+        $assign="SELECT * FROM `hc_templates` WHERE `template`='$temp_tbd'";
+        $result_assign=mysqli_query($connectDB,$assign);
+        $team=mysqli_fetch_assoc($result_assign);
+        $development_assign=$team["development_assign"];
+        $time_req_dev=$team["time_req_dev"];
+    
+        $sqlg="INSERT INTO `assign_task`(`task_name`, `temp_tbd`, `assign_to`, `time_v`, `status_v`,`assign_time`,`assets`) VALUES ('$task_name','$temp_tbd','$development_assign','$time_req_dev','0','$time','$assets')";
+        $resultg=mysqli_query($connectDB,$sqlg);
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -62,11 +70,11 @@ if(isset($_POST["submit"])){
             font-size: 25px;
         }
 
-        .submit{
+        /* .submit{
             position: absolute;
             top: 185px;
             left: 658px;
-        }
+        } */
         .back{
             position: absolute;
             top: 185px;
@@ -85,12 +93,39 @@ if(isset($_POST["submit"])){
         <form action="" method="post">
         <div class="text">You have completed <span style="color:red"><?php echo $row["task"] ?></span></div>
         <div class="text">please share the link for assets</div>
-        
-        <input name="assets" type="text">
-        <button name="submit" class="submit">submit</button>
+        <input name="assets-0" type="text" placeholder="Please put asset link">
+        <div id="asi"></div>
+        <input type="hidden" id="test1" name="input1" >
+        <br>
+        <button style="margin-top:30px" name="submit" class="submit">submit</button>
         </form>
-        <button class="back"><a href="view_v.php">back</a></button>
+        <button style="margin-top:30px"><a href="view_v.php">back</a></button>
+        <button id="add" style="margin-top:30px" onclick="adint()" >Add input</button>
     </div>
+
+
+    <script>
+var j=1;
+function adint(){
+    var assets = document.createElement('input');
+           assets.type = 'text';
+           assets.placeholder = 'DCO:url';
+            assets.name = "col"+j;
+           assets.id = "col"+j;
+    document.getElementById("asi").appendChild(assets);
+
+    j++;
+    var clients = document.getElementById('asi');
+let children = clients.children;
+var values = [];
+for(var i = 0;i<=children.length-1;i++)
+{
+    values.push(children[i].name);
+}
+console.log(values);
+document.getElementById('test1').value=values;
+}
+    </script>
 </body>
 
 </html>
